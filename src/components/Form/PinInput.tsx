6d69520a5279baf63b4d105831d0fc9e38555bbb
angelstreet <joachim_djibril@hotmail.com>
2025-03-01 01:04:43 +0100

    dodo

--- src/components/Form/PinInput.tsx
+++ src/components/Form/PinInput.tsx
@@ -2,4 +2,4 @@
 // The actual implementation has been moved to src/components/common/PinInput
 export * from './common/PinInput';
 
-import { PinInput } from '@/components/Form/PinInput';
+export { PinInput } from './PinInput/common/PinInput';
