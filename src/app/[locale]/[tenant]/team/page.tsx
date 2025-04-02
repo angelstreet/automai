1ee777e180047ece5430b2d6627183b5fd50315b
angelstreet <joachim_djibril@hotmail.com>
2025-04-02 18:43:33 +0200

    team fix title description

--- src/app/[locale]/[tenant]/team/page.tsx
+++ src/app/[locale]/[tenant]/team/page.tsx
@@ -2,26 +2,14 @@ import { getTranslations } from 'next-intl/server';
 import { Suspense } from 'react';
 
 import { getUser } from '@/app/actions/userAction';
-import {  mapAuthUserToUser  } from '@/types/component/userComponentType';
+import { FeaturePageContainer } from '@/components/layout/FeaturePageContainer';
+import { mapAuthUserToUser } from '@/types/component/userComponentType';
 
 import TeamHeader from './_components/TeamHeader';
 import TeamOverviewSkeleton from './_components/TeamOverviewSkeleton';
 import TeamSkeleton from './_components/TeamSkeleton';
 import TeamActionsClient from './_components/client/TeamActionsClient';
 import TeamTabsClient from './_components/client/TeamTabsClient';
-
-// Wrapper component that accepts pageMetadata
-function TeamContent({ user, pageMetadata }) {
-  return (
-    <Suspense fallback={<TeamSkeleton />}>
-      {/* TeamHeader gets details from TeamContext */}
-      <TeamHeader user={user} />
-      <Suspense fallback={<TeamOverviewSkeleton />}>
-        <TeamTabsClient user={user} />
-      </Suspense>
-    </Suspense>
-  );
-}
 
 export default async function TeamPage() {
   const t = await getTranslations('team');
@@ -30,16 +18,21 @@ export default async function TeamPage() {
   const authUser = await getUser();
   const user = authUser ? mapAuthUserToUser(authUser) : null;
 
-  // Using the simplified pattern with pageMetadata
+  // Using FeaturePageContainer directly like repositories page
   return (
-    <TeamContent 
-      user={user}
-      pageMetadata={{
-        title: t('title'),
-        description: t('description'),
-        actions: <TeamActionsClient />
-      }}
-    />
+    <FeaturePageContainer
+      title={t('title')}
+      description={t('description')}
+      actions={<TeamActionsClient />}
+    >
+      <Suspense fallback={<TeamSkeleton />}>
+        {/* TeamHeader gets details from TeamContext */}
+        <TeamHeader user={user} />
+        <Suspense fallback={<TeamOverviewSkeleton />}>
+          <TeamTabsClient user={user} />
+        </Suspense>
+      </Suspense>
+    </FeaturePageContainer>
   );
 }
 
