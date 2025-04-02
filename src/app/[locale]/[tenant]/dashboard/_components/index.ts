e99e022ce66ed3c0962eb70e1989b6560202c70a
ndoye.joachim <ndoye.joachim@gmail.com>
2025-04-02 12:34:02 +0000

    renaming

--- src/app/[locale]/[tenant]/dashboard/_components/index.ts
+++ src/app/[locale]/[tenant]/dashboard/_components/index.ts
@@ -14,18 +14,9 @@ export { DashboardTabContentCard } from './DashboardTabContentCard';
 export { DashboardOverview } from './DashboardOverview';
 export { DashboardRecentSales } from './DashboardRecentSales';
 
-// Client components - backward compatibility
-export { DashboardHeaderClient as DashboardHeader } from './client/DashboardHeaderClient';
-export { DashboardTabsClient as DashboardTabs } from './client/DashboardContent';
-export { DashboardOverviewClient as Overview } from './client/DashboardOverviewClient';
-export { DashboardRecentSalesClient as RecentSales } from './client/DashboardRecentSalesClient';
-export { DashboardMainContentClient } from './client/DashboardMainContentClient';
-export { DashboardStatsCardsClient } from './client/DashboardStatsCardsClient';
-export { DashboardTabContentCardClient } from './client/DashboardTabContentCardClient';
 
 // New client exports with consistent naming
-export { DashboardHeaderClient } from './client/DashboardHeaderClient';
-export { DashboardTabsClient } from './client/DashboardContent';
+export { DashboardContent } from './client/DashboardContent';
 export { DashboardOverviewClient } from './client/DashboardOverviewClient';
 export { DashboardRecentSalesClient } from './client/DashboardRecentSalesClient';
 export { DashboardMainContentClient } from './client/DashboardMainContentClient';
--- src/app/[locale]/[tenant]/dashboard/page.tsx
+++ src/app/[locale]/[tenant]/dashboard/page.tsx
@@ -2,13 +2,13 @@ import { getTranslations } from 'next-intl/server';
 
 import { FeaturePageContainer } from '@/components/layout/FeaturePageContainer';
 
-import { DashboardTabsClient } from './_components/client/DashboardContent';
+import { DashboardContent } from './client/DashboardContent';
 
 export default async function DashboardPage() {
   const t = await getTranslations('Dashboard');
   return (
     <FeaturePageContainer title={t('title')} description={t('description')} actions={null}>
-      <DashboardContent/>
+      <DashboardContent />
     </FeaturePageContainer>
   );
 }
