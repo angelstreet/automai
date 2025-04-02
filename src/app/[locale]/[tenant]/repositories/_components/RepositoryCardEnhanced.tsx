181fe05a320215e3fe764a8e8dd45ba5475498c8
angelstreet <joachim_djibril@hotmail.com>
2025-04-02 23:11:57 +0200

    remove duplicate

--- src/app/[locale]/[tenant]/repositories/_components/RepositoryCardEnhanced.tsx
+++
@@ -1,5 +0,0 @@
-import { EnhancedRepositoryCard } from './EnhancedRepositoryCard';
-
-export function RepositoryCardEnhanced(props: React.ComponentProps<typeof EnhancedRepositoryCard>) {
-  return <EnhancedRepositoryCard {...props} />;
-}
