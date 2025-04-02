f8878e0721f3ff1de34297ffb219e5c8d53bcd59
ndoye.joachim <ndoye.joachim@gmail.com>
2025-04-02 07:52:44 +0000

    sidebar

--- src/components/sidebar/SidebarRail.tsx
+++ src/components/sidebar/SidebarRail.tsx
@@ -20,8 +20,7 @@ export const SidebarRail = React.forwardRef<HTMLButtonElement, React.ComponentPr
           'bg-accent/10 hover:bg-accent/20',
           'after:absolute after:inset-y-0 after:left-1/2 after:w-[2px] after:bg-accent/20 hover:after:bg-accent/40',
           'group-data-[side=left]:-right-4 group-data-[side=right]:left-0 sm:flex',
-          '[[data-side=left]_&]:cursor-w-resize [[data-side=right]_&]:cursor-e-resize',
-          '[[data-side=left][data-state=collapsed]_&]:cursor-e-resize [[data-side=right][data-state=collapsed]_&]:cursor-w-resize',
+          'cursor-pointer', // Override resize cursors with simple pointer
           'group-data-[collapsible=offcanvas]:translate-x-0 group-data-[collapsible=offcanvas]:after:left-full',
           '[[data-side=left][data-collapsible=offcanvas]_&]:-right-2',
           '[[data-side=right][data-collapsible=offcanvas]_&]:-left-2',
