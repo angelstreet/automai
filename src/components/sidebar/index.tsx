3c5c0ef54122e3d0a38e033ca5981a0cadc25be9
ndoye.joachim <ndoye.joachim@gmail.com>
2025-04-03 07:23:34 +0000

    remove obsolete files

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,22 +1,17 @@
 // Core sidebar exports
 export { default as Sidebar } from './Sidebar';
-export { SidebarClient } from './SidebarClient'; 
+export { SidebarClient } from './SidebarClient';
 export { SidebarRail } from './SidebarRail';
 export { SidebarTrigger } from './SidebarTrigger';
 
 // Layout components
-export {
-  SidebarContent,
-  SidebarFooter,
-  SidebarHeader,
-  SidebarSeparator,
-} from './SidebarLayout';
+export { SidebarContent, SidebarFooter, SidebarHeader, SidebarSeparator } from './SidebarLayout';
 
 // Skeleton component
 export { SidebarSkeleton } from './SidebarSkeleton';
 
 // Navigation components
-export { 
+export {
   SidebarNavigation,
   SidebarMenu,
   SidebarMenuItem,
