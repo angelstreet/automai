3086ee467166c20facffa50a9f1f39a4f819e7f2
angelstreet <joachim_djibril@hotmail.com>
2025-04-11 21:17:48 +0200

    poll10s

--- runner/index.js
+++ runner/index.js
@@ -180,6 +180,6 @@ async function setupSchedules() {
 }
 
 // Poll queue every 5 seconds
-setInterval(processJob, 5000);
+setInterval(processJob, 10000);
 setupSchedules().catch((err) => console.error('Setup schedules failed:', err));
 console.log('Worker running...');
