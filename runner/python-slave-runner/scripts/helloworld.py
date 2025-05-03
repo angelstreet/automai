46be17ee480835a72310a94f05672bc49b88795d
angelstreet <joachim_djibril@hotmail.com>
2025-05-03 23:20:31 +0200

    var env testing

--- runner/python-slave-runner/scripts/helloworld.py
+++ runner/python-slave-runner/scripts/helloworld.py
@@ -1,8 +1,11 @@
 import requests
+import os
 
 def main():
     print("Hello, World!")
     print(f"Requests version: {requests.__version__}")
+    ftp_server = os.getenv('FTP_SERVER', 'Not set')
+    print(f"FTP_SERVER: {ftp_server}")
     print("Test Success")
     return 0
 
