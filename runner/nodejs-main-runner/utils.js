2a0989d5e83ef0eafdea03c354b159cf66714c09
AngelStreet <ndoye.joachim@gmail.com>
2025-05-05 13:09:33 +0200

    util.js

---
+++ runner/nodejs-main-runner/utils.js
@@ -0,0 +1,17 @@
+const crypto = require('crypto');
+
+const ALGORITHM = 'aes-256-gcm';
+
+function decrypt(encryptedData, keyBase64) {
+  const key = Buffer.from(keyBase64, 'base64');
+  const [ivHex, authTagHex, encrypted] = encryptedData.split(':');
+  const iv = Buffer.from(ivHex, 'hex');
+  const authTag = Buffer.from(authTagHex, 'hex');
+  const decipher = crypto.createDecipheriv(ALGORITHM, key, iv);
+  decipher.setAuthTag(authTag);
+  let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+  decrypted += decipher.final('utf8');
+  return decrypted;
+}
+
+module.exports = { decrypt, ALGORITHM };
