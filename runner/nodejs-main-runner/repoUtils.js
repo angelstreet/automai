23064c45b115ce2442cec2efac8d51a75eb56f01
AngelStreet <ndoye.joachim@gmail.com>
2025-05-05 22:50:28 +0200

    rm node fetch

--- runner/nodejs-main-runner/repoUtils.js
+++ runner/nodejs-main-runner/repoUtils.js
@@ -1,18 +1,20 @@
-const fetch = require('node-fetch');
+const axios = require('axios');
 
 async function pingRepository(repoUrl) {
   try {
     console.log(`[pingRepository] Checking availability of repository: ${repoUrl}`);
-    const response = await fetch(process.env.REPOSITORY_PING_URL, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${process.env.REPOSITORY_PING_TOKEN}`,
+    const response = await axios.post(
+      process.env.REPOSITORY_PING_URL,
+      { repo_url: repoUrl },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${process.env.REPOSITORY_PING_TOKEN}`,
+        },
       },
-      body: JSON.stringify({ repo_url: repoUrl }),
-    });
+    );
 
-    if (!response.ok) {
+    if (response.status !== 200) {
       console.error(
         `[pingRepository] Failed to check repository ${repoUrl}: ${response.status} ${response.statusText}`,
       );
