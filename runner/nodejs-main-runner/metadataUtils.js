7bc01763aba28db62443fa6cd0f6002fbe62f5d3
AngelStreet <ndoye.joachim@gmail.com>
2025-05-07 11:21:47 +0200

    t

--- runner/nodejs-main-runner/metadataUtils.js
+++ runner/nodejs-main-runner/metadataUtils.js
@@ -1,15 +1,13 @@
 const fs = require('fs');
-const path = require('path');
 
 /**
- * Write script execution metadata to metadata.json in the script folder.
- * @param {string} scriptFolderPath - The path to the script folder.
+ * Write script execution metadata to the specified file path.
+ * @param {string} metadataPath - The full path to the metadata file.
  * @param {object} metadata - The metadata object (job_id, script_id, script_name, script_path, parameters, start_time, end_time, status).
  */
-function writeScriptMetadata(scriptFolderPath, metadata) {
-  const metadataPath = path.join(scriptFolderPath, 'metadata.json');
+function writeScriptMetadata(metadataPath, metadata) {
   fs.writeFileSync(metadataPath, JSON.stringify(metadata, null, 2));
-  console.log(`[@metadataUtils] Wrote metadata.json to ${metadataPath}`);
+  console.log(`[@metadataUtils] Wrote script metadata to ${metadataPath}`);
 }
 
 /**
