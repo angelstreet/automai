18488036320a65ecb09bb68cb797d81d4df7d21c
AngelStreet <ndoye.joachim@gmail.com>
2025-05-26 23:43:46 +0200

    test

--- src/app/[locale]/[tenant]/virtualpytest/page.tsx
+++ src/app/[locale]/[tenant]/virtualpytest/page.tsx
@@ -3,6 +3,8 @@ import VirtualpytestContent from './_components/VirtualpytestContent';
 export default async function VirtualPyTestPage() {
   
   return ( 
+    <div className="overflow-hidden h-screen">
       <VirtualpytestContent />
+    </div>
   );
 }
