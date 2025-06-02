bb4a9d667d4f5d956d284e2d81cfb34f5fa78bad
AngelStreet <ndoye.joachim@gmail.com>
2025-06-02 09:56:06 +0200

    scrollbar

--- virtualpytest/src/web/components/user-interface/VerificationEditor.tsx
+++ virtualpytest/src/web/components/user-interface/VerificationEditor.tsx
@@ -184,13 +184,33 @@ export const VerificationEditor: React.FC<VerificationEditorProps> = ({
           },
         },
       }}>
-        <NodeVerificationsList
-          verifications={verifications}
-          availableActions={verificationActions}
-          onVerificationsChange={handleVerificationsChange}
-          loading={loading}
-          error={error}
-        />
+        <Box sx={{ 
+          height: '250px', 
+          overflowY: 'auto',
+          overflowX: 'hidden',
+          '&::-webkit-scrollbar': {
+            width: '6px',
+          },
+          '&::-webkit-scrollbar-track': {
+            background: 'rgba(255,255,255,0.1)',
+            borderRadius: '3px',
+          },
+          '&::-webkit-scrollbar-thumb': {
+            background: 'rgba(255,255,255,0.3)',
+            borderRadius: '3px',
+            '&:hover': {
+              background: 'rgba(255,255,255,0.5)',
+            },
+          },
+        }}>
+          <NodeVerificationsList
+            verifications={verifications}
+            availableActions={verificationActions}
+            onVerificationsChange={handleVerificationsChange}
+            loading={loading}
+            error={error}
+          />
+        </Box>
       </Box>
 
       {/* Action Buttons */}
