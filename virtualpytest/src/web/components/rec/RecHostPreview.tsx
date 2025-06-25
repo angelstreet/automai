2e37bfc14eaf83b696509b6437bc6649a1491a23
AngelStreet <ndoye.joachim@gmail.com>
2025-06-25 17:24:48 +0200

    200

--- virtualpytest/src/web/components/rec/RecHostPreview.tsx
+++ virtualpytest/src/web/components/rec/RecHostPreview.tsx
@@ -215,7 +215,7 @@ export const RecHostPreview: React.FC<RecHostPreviewProps> = ({
   return (
     <Card
       sx={{
-        height: 220,
+        height: 200,
         display: 'flex',
         flexDirection: 'column',
         position: 'relative',
