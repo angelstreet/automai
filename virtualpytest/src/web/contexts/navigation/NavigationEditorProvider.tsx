6e4d547fbd3755aec0ecc9a07350ff9ee815eecf
AngelStreet <ndoye.joachim@gmail.com>
2025-06-28 12:58:55 +0200

    fix nav

--- virtualpytest/src/web/contexts/navigation/NavigationEditorProvider.tsx
+++ virtualpytest/src/web/contexts/navigation/NavigationEditorProvider.tsx
@@ -4,6 +4,7 @@ import { DeviceDataProvider } from '../device/DeviceDataContext';
 import { NavigationActionsProvider } from './NavigationActionsContext';
 import { NavigationFlowProvider } from './NavigationFlowContext';
 import { NavigationNodesProvider } from './NavigationNodesContext';
+import { NavigationStateProvider } from './NavigationStateContext';
 import { NavigationUIProvider } from './NavigationUIContext';
 
 interface NavigationEditorProviderProps {
@@ -17,13 +18,15 @@ interface NavigationEditorProviderProps {
 export const NavigationEditorProvider: React.FC<NavigationEditorProviderProps> = ({ children }) => {
   return (
     <DeviceDataProvider>
-      <NavigationNodesProvider>
-        <NavigationUIProvider>
-          <NavigationFlowProvider>
-            <NavigationActionsProvider>{children}</NavigationActionsProvider>
-          </NavigationFlowProvider>
-        </NavigationUIProvider>
-      </NavigationNodesProvider>
+      <NavigationStateProvider>
+        <NavigationNodesProvider>
+          <NavigationUIProvider>
+            <NavigationFlowProvider>
+              <NavigationActionsProvider>{children}</NavigationActionsProvider>
+            </NavigationFlowProvider>
+          </NavigationUIProvider>
+        </NavigationNodesProvider>
+      </NavigationStateProvider>
     </DeviceDataProvider>
   );
 };
